b2ceb81e875f8fd1e82b6e78f255026bba273ca3
hanashiro <felipeaah@gmail.com>
2020-03-20 09:28:45 -0300

    aula-03-03

--- admin/functions/index.js
+++ admin/functions/index.js
@@ -1,8 +1,27 @@
 var admin = require("firebase-admin");
-
 var serviceAccount = require("./private-keys.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://my-app-auth-78a32.firebaseio.com"
 });
+
+const token = '';
+
+admin.messaging().sendToDevice(token, {
+  data: {
+    nome: 'Treinaweb'
+  }
+},
+{
+  priority: 'high',
+  timeToLive: 60 * 60 * 24,
+  collapseKey: 'ab'
+})
+.then((response) => {
+  console.log(response);
+  process.exit();
+})
+.catch((error) => {
+  console.log('Erro no envio')
+})
