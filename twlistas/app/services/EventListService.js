e1ec1906db62bf427412a56e340eb9e8934537e3
hanashiro <felipeaah@gmail.com>
2020-03-20 08:37:37 -0300

    aula-02-03

--- twlistas/app/services/EventListService.js
+++ twlistas/app/services/EventListService.js
@@ -1,10 +1,12 @@
+import messaging from '@react-native-firebase/messaging';
 import AsyncStorage from '@react-native-community/async-storage';
 
 const listKey = '@eventList';
 const defaultList = [
-    {id: '1', body: {name: 'Atualizações', isOn: true}},
-    {id: '2', body: {name: 'Promoções', isOn: false}},
-]
+    {id: '1', body: {name: 'Atualizações', isOn: true, topic: 'atualizacoes'}},
+    {id: '2', body: {name: 'Promoções', isOn: false, topic: 'promocoes'}},
+];
+
 
 export const EventListService = {
     async getList(){
@@ -19,9 +21,24 @@ export const EventListService = {
     async setItem(newItem){
         try{
             const list = await EventListService.getList();
+            console.log(list)
             const newList = list.map(item => item.id !== newItem.id ? item:newItem);
             AsyncStorage.setItem(listKey, JSON.stringify(newList));
+            EventListService.updateEventsNotifications();
             return newList;
+        }catch(error){}
+    },
+    async updateEventsNotifications(){
+        try{
+            const eventList = await EventListService.getList();
+            messaging().subscribeToTopic('all');
+            eventList.forEach(event => {
+                if(event.body.isOn){
+                    messaging().subscribeToTopic(event.body.topic);
+                }else{
+                    messaging().unsubscribeFromTopic(event.body.topic);
+                }
+            })
         }catch(error){}
     }
 }
--- twlistas/index.js
+++ twlistas/index.js
@@ -5,5 +5,8 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+import {EventListService} from './app/services/EventListService';
 
 AppRegistry.registerComponent(appName, () => App);
+
+EventListService.updateEventsNotifications();
